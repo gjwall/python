6a69a239901726a349750a3aca7a274080434ae6
gjwall <gjwall@users.noreply.github.com>
2017-03-05 20:29:46 +0000

    addional os work

--- Training/PythonProjects/access_os.py
+++ Training/PythonProjects/access_os.py
@@ -8,6 +8,7 @@ Created on 5 Mar 2017
 
 import os
 import platform
+import subprocess as sub
 
 my_login = os.getlogin()
 
@@ -20,3 +21,35 @@ print ("My OS = " + my_system + " " + os.name)
 # print (os.umask())
 
 # print (os.uname())
+
+print (os.getpid())
+
+print (os.getppid())
+
+print (os.getcwd())
+
+print (len(os.environ))
+
+for key in os.environ.keys():
+    print ("%30s %s \n" % (key,os.environ[key]))    
+    
+# Old way
+# sub.call(["cmd", "/c", "dir", "/b"])
+
+sub.run(["cmd", "/c", "dir", "/b"])
+
+print("POpen with Shell")
+sub.Popen(["cmd /c dir "], shell = True)
+
+print ("POpen with shell and pipe")
+my_dir = sub.Popen(["cmd /c dir "], shell = True, stdout=sub.PIPE)
+
+for f in my_dir.stdout: 
+    print (f)
+#b' means a bytestring
+
+print("\nListing a directory in Python\n")
+print(os.listdir("."))
+print (os.stat("ch1_ex1.py"))
+
+    
